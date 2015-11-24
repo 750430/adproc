7281035061e950d7d8f3500f1254075a01b4d035
unknown <up750430@myport.ac.uk>
2015-11-24 09:44:56 +0000

    merge

---
+++ src/courswork/processOrder.java
@@ -0,0 +1,29 @@
+package courswork;
+
+/**
+ * ADPROC Coursework 2015.
+ *
+ * ORDER SYSTEM FOR A PIPE-SELLING COMPANY
+ *
+ * @authors 750430 & 750148
+ */
+
+public class processOrder {
+    
+    private Pipe pipe;
+    private double areaOfPipe;
+    
+    public processOrder(Pipe pipe, double areaOfPipe) {
+        Pipe = pipe;
+        areaOfPipe = areaOfPipe;
+    }
+        
+    
+
+
+
+
+
+
+
+}
